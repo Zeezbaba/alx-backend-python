d10d88339ba61d6763c8d1955efacdaff7efcee5
Zeezbaba <Azeez_okeleji>
2024-07-01 12:50:29 +0000

    Unittests task solution2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -43,5 +43,28 @@ class TestAccessNestedMap(unittest.TestCase):
         # self.assertEqual(f"KeyError('{expected}')", repr(err.exception))
 
 
+class TestGetJson(unittest.TestCase):
+    """tests get_json function"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """test that utils.get_json returns the expected result"""
+        with patch('utils.requests.get') as mocked_get:
+            # Create a Mock response object with a json method
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mocked_get.return_value = mock_response
+
+            # Call the function
+            result = get_json(test_url)
+
+            # Assert the get method was called exactly once with test_url
+            mocked_get.assert_called_once_with(test_url)
+            # Assert the result is equal to test_payload
+            self.assertEqual(result, test_payload)
+
+
 if __name__ == "__main__":
     unittest.main()
