9e8959dbbec5a03736768e11bbf09fd4c9c747e2
Zeezbaba <Azeez_okeleji>
2024-07-01 21:59:12 +0000

    Unittests clients

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -12,7 +12,7 @@ GithubOrgClient, in this order:
 """
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -29,19 +29,20 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.return_value = expected
         client = GithubOrgClient(org_name)
 
+        self.assertEqual(client.org, expected)
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}",
             )
-        self.assertEqual(client.org, expected)
+        # self.assertEqual(client.org, expected)
 
     def test_public_repos_url(self):
         """Testing"""
         with patch.object(GithubOrgClient, 'org',
-                          new_callable=property) as mock_org:
+                          new_callable=PropertyMock) as mock_org:
             mock_org.return_value = {
                 "repos_url": "https://api.github.com/orgs/google/repos",
                 }
-            client = GithubOrgClient("google")
+            # client = GithubOrgClient("google")
             self.assertEqual(
                 GithubOrgClient("google")._public_repos_url,
                 "https://api.github.com/orgs/google/repos",
