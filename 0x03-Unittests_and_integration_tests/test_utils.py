9193d9129307704ba8434b9ba97b1ac8b63f8601
Zeezbaba <Azeez_okeleji>
2024-07-01 13:08:19 +0000

    Unittests task solution2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -50,7 +50,7 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json(self, test_url, test_payload):
+    def test_get_json(self, test_url, expected):
         """test that utils.get_json returns the expected result"""
         mock_response = Mock()
         mock_response.json.return_value = expected
