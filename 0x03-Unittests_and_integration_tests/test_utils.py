7211035c450bc8aa3638104aa001aca55ef99878
Zeezbaba <Azeez_okeleji>
2024-07-01 13:03:58 +0000

    Unittests task solution2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,8 +15,9 @@ function returns the expected result.The body of the test method
 should not be longer than 2 lines."""
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
