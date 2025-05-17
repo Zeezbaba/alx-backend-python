4bd0bf730052ab00b37f12e472f3253581c786c2
Zeezbaba <okelejitimi93@gmail.com>
2025-05-17 13:44:16 +0100

    generator that streams rows from an SQL database

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,24 @@
+import mysql.connector
+def stream_users():
+    """function that uses a generator to fetch rows
+    one by one from the user_data table. You must use
+    the Yield python generator
+    """
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='pwd123',
+            database='ALX_prodev'
+        )
+        cursor = connection.cursor(Dictionary=true)
+        cursor.execute("SELECT user_id, name, email, age FROM user_data")
+
+        for row in cursor:
+            # convert decimal Age into integer
+            row['age'] = int(row['age'])
+            yield row
+        cursor.close()
+        connection.close()
+    except mysql.connector.Error as e:
+        print(f"Database Error: {e}")
