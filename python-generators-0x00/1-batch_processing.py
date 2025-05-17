0a2b4a83410f1ce255696f8f1b862cf9b0186bce
Zeezbaba <okelejitimi93@gmail.com>
2025-05-17 14:33:31 +0100

    Batch processing Large Data

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,35 @@
+import mysql.connector
+from decimal import Decimal
+
+def stream_users_in_batches(batch_size):
+    """fetches rows in batches"""
+    db_connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='pwd123',
+        database='ALX_prodev'
+    )
+    cursor = db_connection.cursor(dictionary=True)
+
+    cursor.execute("SELECT COUNT(*) FROM user_data")
+    total = cursor.fetchone("COUNT(*)")
+
+    for offset in range(0, total, batch_size):
+        cursor.execute(
+            "SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset)
+        )
+        batch = cursor.fetchall()
+        yield batch
+
+    cursor.close()
+    db_connection.close()
+
+def batch_processing(batch_size):
+    """processes each batch to filter users over the age of 25"""
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            age = user['age']
+            if isinstance(age, decimal):
+                age = int(age)
+            if age > 25:
+                print(user)
