893d015f1bc38bdecec262331a45e759a5631c58
Zeezbaba <okelejitimi93@gmail.com>
2025-05-27 15:46:52 +0100

    Define data Models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,17 +1,52 @@
+import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.conf import settings
 
-class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
+class UserManager(BaseUserManager):
+    """custom usermanager to handle user creation properly"""
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email must be provided')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+            
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User model"""
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
 
 class Conversation(models.Model):
     """conversation model"""
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_names='conversation')
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_names='conversation')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"conversation {self.id}"
+        return f"conversation {self.conversation_id}"
 
 class Message(models.Model):
     """ message model """
